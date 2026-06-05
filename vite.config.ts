bf27601cec43f3bcd4680aad9fb63636f3ee50b6
clvnkhr <calvin_khor@hotmail.com>
2026-06-05 11:49:25 +0100

    add action for deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { foldkit } from '@foldkit/vite-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/foldkid/',
   plugins: [foldkit()],
   optimizeDeps: {
     entries: ['src/**/*.ts'],
